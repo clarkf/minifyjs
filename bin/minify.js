50cbe2f8f948fb118fc4e0bec16858f6a7fd0b9c
Clark Fischer <clark.fischer@gmail.com>
2011-01-20 19:05:18 -0800

    Significant updates. Restructuring, testing. A lot is better.

--- bin/minify.js
+++ bin/minify.js
@@ -1,99 +1,90 @@
-#! /usr/bin/env node
-// -*- js2 -*-
-
 global.sys = require(/^v0\.[012]/.test(process.version) ? "sys" : "util");
 var fs = require("fs");
 var minify = require("minifyjs");
 
 var options = {
-        mode: 'minify',
-        level: 3,
-        engine: undefined,
-        verbose: false,
-        output: true            // stdout
+    mode: 'minify',
+    level: 3,
+    verbose: false,
+    output: true            // stdout
 };
 
 var args = Array.prototype.slice.call(process.argv, 2);
 var filename;
 
 out: while (args.length > 0) {
-        var v = args.shift();
-        switch (v) {
-            case "-b":
+    var v = args.shift();
+    switch (v) {
+        case "-b":
             case "--beautify":
                 options.mode = 'beautify';
-                break;
-            case "-m":
+        break;
+        case "-m":
             case "--minify":
                 options.mode = 'minify';
-                break;
-            case "-l":
+        break;
+        case "-l":
             case "--level":
                 options.level = Number(args.shift());
-                break;
-            case "--engine":
+        break;
+        case "--engine":
             case "-e":
                 options.engine = args.shift();
-                break;
-            case "--verbose":
+        break;
+        case "--verbose":
             case "-v":
-                options.verbose = true;
-                break;
-            case "-o":
+                options.log = require('util').debug;
+        break;
+        case "-o":
             case "--output":
                 options.output = args.shift();
-                break;
-            default:
-                filename = v;
-                break out;
-        }
+        break;
+        default:
+            filename = v;
+        break out;
+    }
 }
 
 if (filename) {
-        fs.readFile(filename, "utf8", function(err, text){
-                processIt(text,output);
-        });
+    fs.readFile(filename, "utf8", function(err, text){
+        processIt(text,output);
+    });
 } else {
-        var stdin = process.openStdin();
-        stdin.setEncoding("utf8");
-        var text = "";
-        stdin.on("data", function(chunk){
-                text += chunk;
-        });
-        stdin.on("end", function() {
-                processIt(text,output);
-        });
+    var stdin = process.openStdin();
+    stdin.setEncoding("utf8");
+    var text = "";
+    stdin.on("data", function(chunk){
+        text += chunk;
+    });
+    stdin.on("end", function() {
+        processIt(text,output);
+    });
 }
 
-function output(text) {
-        var out;
-        if (options.output === true) {
-                out = process.stdout;
-        } else {
-                out = fs.createWriteStream(options.output, {
-                        flags: "w",
-                        encoding: "utf8",
-                        mode: 0644
-                });
-        }
-        out.write(text);
-        out.end();
+function output(error, text) {
+    var out;
+    if (options.output === true) {
+        out = process.stdout;
+    } else {
+        console.log('Writing to options.output');
+        out = fs.createWriteStream(options.output, {
+            flags: "w",
+            encoding: "utf8",
+            mode: 0644
+        });
+    }
+    out.write(text.toString());
+    out.end();
 };
 
 // --------- main ends here.
 
 function processIt(code, cb) {
-        try {
-				minify[options.mode](
-					code,
-					cb,
-					options.engine,
-					options.level,
-					options.verbose ? sys.error : undefined
-				);
-        } catch(ex) {
-                sys.debug(ex.stack);
-                sys.debug(sys.inspect(ex));
-                sys.debug(JSON.stringify(ex));
-        }
+    try {
+        minify[options.mode](code, options, cb);
+    } catch(ex) {
+        sys.debug(ex.stack);
+        sys.debug(sys.inspect(ex));
+        sys.debug(JSON.stringify(ex));
+    }
 };
--- index.js
+++
@@ -1,1 +0,0 @@
-exports.minify = require("./lib/minify");
