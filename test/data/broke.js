2392a734713eb7abc61797fe46b7518f00ec8646
Clark Fischer <clark.fischer@gmail.com>
2011-09-13 18:23:20 -0700

    Added fake test data.

---
+++ test/data/broke.js
@@ -0,0 +1,8 @@
+/This is a comment that is missing something. ;)
+
+)function thisLine Shouldn't Start With AParenthesis(ha, ha, ha) {{
+    R0c|{ 0/\/;!
+}}
+
+
+return flashbang;
---
+++ test/data/valid.js
@@ -0,0 +1,15 @@
+var hi = "HI",
+    num = 12345,
+    fl = 123.53;
+
+function myFunction (ar, gu, ment, s) {
+    //A comment!
+    return "SURPRISE".toLowerCase();
+}
+
+globalFunction = function aDifferentName (la, ha, da) {
+    for (i = 1; i < 1234; i++) {
+        console.log("Dumb loop!");
+    }
+    return Math.Infinity;
+}
